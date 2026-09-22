f39cc3d6a3843278f45db684a83c7eafa8ca2bac
stepbrobd <stepbrobd@users.noreply.github.com>
2026-09-22 20:17:39 +0000

    Deploying to gh-pages from @ nix-community/colmena@96b41fa10532d9119a1f83045c29b3344f623a40 🚀

--- unstable/searcher-c2a407aa.js
+++ unstable/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e9b0cd50.js',
+                path_to_root + 'searchindex-4598b65c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
